6bab93e7acab17a2e7702c9312cf56503071602f
George Paul <george.allen.paul.3@gmail.com>
2020-01-28 23:04:49 -0500

    Add gh-pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 
 module.exports = {
+  pathPrefix: "/warforgravesend",
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
